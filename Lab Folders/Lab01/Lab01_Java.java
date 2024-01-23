cfee4c625ec5689ce8e0d8937f332385dde0c415
Jhan Valenzuela <148958644+TPTM32-Jhan@users.noreply.github.com>
2024-01-23 16:46:24 +0000

    Update Lab01_Java.java
    Changes made by student

--- Lab Folders/Lab01/Lab01_Java.java
+++ Lab Folders/Lab01/Lab01_Java.java
@@ -4,7 +4,7 @@ void testMethod1()
   {
   //First command x = y + z
   //Second commnad print(x)
-  //Edit made by student
+  //Edit made by JhanValenzuela_210082920
 
 
   //Last line print('Good bye')
